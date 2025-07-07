1e851d10440eb5669219b83b545a167428dee437
Hasnine Mamud <hasninehemel85@gmail.com>
2025-07-07 11:25:35 +0600

    Updated media file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,9 +82,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
